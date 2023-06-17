1a4dc78ccf8ce64e6cba8107b537b8fd8703fbff
Matheus Aurélio <matheusaurelio2004@gmail.com>
2023-06-17 11:31:58 -0300

    refactor: Add custom hook useOrders

--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -1,53 +1,10 @@
-import { useEffect, useState } from 'react';
-import socketIo from 'socket.io-client';
-
 import { OrdersBoard } from './components/OrdersBoard';
 
-import { Order } from '../../types/Order';
-
-import { api } from '../../services/api';
-
 import { Container } from './styles';
+import useOrders from './useOrders';
 
 export function Orders() {
-  const [orders, setOrders] = useState<Order[]>([]);
-
-  useEffect(() => {
-    const socket = socketIo('http://localhost:3001', {
-      transports: ['websocket'],
-    });
-
-    socket.on('orders@new', (order) => {
-      setOrders((prevState) => [...prevState, order]);
-    });
-  }, []);
-
-  async function getOrders() {
-    try {
-      const { data } = await api.get(`${api.defaults.baseURL}/orders`);
-      setOrders(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    getOrders();
-  }, []);
-
-  const waiting = orders.filter((order) => order.status === 'WAITING');
-  const inProduction = orders.filter((order) => order.status === 'IN_PRODUCTION');
-  const done = orders.filter((order) => order.status === 'DONE');
-
-  function handleCancelOrder(orderId: string) {
-    setOrders((prevState) => prevState.filter((order) => order._id !== orderId));
-  }
-
-  function handleOrderStatusChange(orderId: string, status: Order['status']) {
-    setOrders((prevState) => prevState.map((order) => (
-      order._id === orderId ? { ...order, status } : order
-    )));
-  }
+  const { waiting, inProduction, done, handleCancelOrder, handleOrderStatusChange } = useOrders();
 
   return (
     <Container>
---
+++ fe/src/components/Orders/useOrders.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import socketIo from 'socket.io-client';
+
+import { Order } from '../../types/Order';
+
+import { api } from '../../services/api';
+
+export default function useOrders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    const socket = socketIo('http://localhost:3001', {
+      transports: ['websocket'],
+    });
+
+    socket.on('orders@new', (order) => {
+      setOrders((prevState) => [...prevState, order]);
+    });
+  }, []);
+
+  async function getOrders() {
+    try {
+      const { data } = await api.get(`${api.defaults.baseURL}/orders`);
+      setOrders(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const waiting = orders.filter((order) => order.status === 'WAITING');
+  const inProduction = orders.filter((order) => order.status === 'IN_PRODUCTION');
+  const done = orders.filter((order) => order.status === 'DONE');
+
+  function handleCancelOrder(orderId: string) {
+    setOrders((prevState) => prevState.filter((order) => order._id !== orderId));
+  }
+
+  function handleOrderStatusChange(orderId: string, status: Order['status']) {
+    setOrders((prevState) => prevState.map((order) => (
+      order._id === orderId ? { ...order, status } : order
+    )));
+  }
+
+  return {
+    waiting,
+    inProduction,
+    done,
+    handleCancelOrder,
+    handleOrderStatusChange,
+  };
+}
